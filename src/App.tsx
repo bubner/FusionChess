dfa4d7ca0904597d2284d0e0416a85bd9e4e7800
Lucas Bubner <lkbubner@proton.me>
2023-03-11 18:36:52 +1030

    Working chess board

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,45 @@
-import { useState } from "react";
-import { Chess } from "chess.js";
+import { useState, useEffect } from "react";
+import { Chess, Square } from "chess.js";
 import { Chessboard } from "react-chessboard";
 import "./App.css";
 
 function App() {
     const [game, setGame] = useState(new Chess());
+    const [fen, setFen] = useState(game.fen());
+    const [boardWidth, setBoardWidth] = useState(window.innerHeight - 100);
 
-    function makeAMove(move: any) {
-        const gameCopy = game;
-        const result = gameCopy.move(move);
-        setGame(gameCopy);
-        return result; // null if the move was illegal, the move object if the move was legal
-      }
-    
-      function makeRandomMove() {
-        const possibleMoves = game.moves();
-        if (game.isGameOver() || game.isDraw() || possibleMoves.length === 0) return; // exit if the game is over
-        const randomIndex = Math.floor(Math.random() * possibleMoves.length);
-        makeAMove(possibleMoves[randomIndex]);
-      }
-    
-      function onDrop(sourceSquare: any, targetSquare: any) {
-        const move = makeAMove({
-          from: sourceSquare,
-          to: targetSquare,
-          promotion: "q", // always promote to a queen for example simplicity
-        });
-    
-        // illegal move
-        if (move === null) return false;
-        setTimeout(makeRandomMove, 200);
+    // Force a rerender if the screen dimensions change
+    useEffect(() => {
+        const handleResize = () => {
+            setBoardWidth(window.innerHeight - 100);
+        };
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    function onDrop(sourceSquare: Square, targetSquare: Square) {
+        let copy = game;
+        try {
+            copy.move({
+                from: sourceSquare,
+                to: targetSquare,
+                promotion: "q",
+            });
+        } catch (Error) {
+            return false;
+        }
+        setGame(copy);
+        // Trigger a re-render by updating fen state, as updating object will not trigger it
+        setFen(copy.fen());
+        setBoardWidth(window.innerHeight - 100);
         return true;
-      }
+    }
 
     return (
         <div className="App">
-            <Chessboard position={game.fen()} onPieceDrop={onDrop} />
+            <div id="board">
+                <Chessboard position={fen} onPieceDrop={onDrop} id="board" boardWidth={boardWidth} />
+            </div>
         </div>
     );
 }
